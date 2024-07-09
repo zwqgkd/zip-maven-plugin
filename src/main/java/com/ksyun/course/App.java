2cf5e3409173a3ce35e475f5a029a56f2c4aaa5c
zwqgkd <zwqgkd@gmail.com>
2024-07-09 17:17:16 +0800

    init

---
+++ src/main/java/com/ksyun/course/App.java
@@ -0,0 +1,13 @@
+package com.ksyun.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
